cc92272849cd8111c62183df5ac6af7fce7df04d
sameermd16 <sameerthemc@gmail.com>
2023-10-05 19:14:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero"
+import CustomerReviews from "./components/CustomerReviews"
+import Footer from './components/Footer'
+import PopularProducts from "./components/PopularProducts"
+import SpecialOffer from "./components/SpecialOffer"
+import Subscribe from "./components/Subscribe"
+import SuperQuality from "./components/SuperQuality"
+import Services from "./components/Services"
+
+function App() {
+
+  return (
+    <mian className="relative">
+      
+    </mian>
+  )
+}
+
+export default App
+  
