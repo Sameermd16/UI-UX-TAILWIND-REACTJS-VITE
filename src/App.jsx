e67568a612943d5df35c131de718e989cb013965
sameermd16 <sameerthemc@gmail.com>
2023-10-07 20:33:32 +0530

    navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import Hero from "./components/Hero"
-import CustomerReviews from "./components/CustomerReviews"
-import Footer from './components/Footer'
-import PopularProducts from "./components/PopularProducts"
-import SpecialOffer from "./components/SpecialOffer"
-import Subscribe from "./components/Subscribe"
-import SuperQuality from "./components/SuperQuality"
-import Services from "./components/Services"
+import Hero from "./sections/Hero"
+import CustomerReviews from "./sections/CustomerReviews"
+import Footer from './sections/Footer'
+import PopularProducts from "./sections/PopularProducts"
+import SpecialOffer from "./sections/SpecialOffer"
+import Subscribe from "./sections/Subscribe"
+import SuperQuality from "./sections/SuperQuality"
+import Services from "./sections/Services"
+import Nav from "./components/Nav"
 
 function App() {
 
   return (
     <mian className="relative">
-      
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
     </mian>
   )
 }
